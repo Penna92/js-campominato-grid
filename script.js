17c215d00f42dace4d47edf28074d9c0d8cada6e
Penna92 <giacomopenna92@gmail.com>
2022-04-04 23:20:02 +0200

    Riscritta funzione che crea le colonne e aggiunta funzione che cambia background delle caselle al click

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let yourSelect = document.getElementById("difficulty");
-console.log(yourSelect.options[yourSelect.selectedIndex].value);
+// console.log(yourSelect.options[yourSelect.selectedIndex].value);
 document.getElementById("button").addEventListener("click", stampareGriglia);
 
 // FUNZIONE CHE STAMPA LA GRIGLIA
 function stampareGriglia() {
+    
   //CONDIZIONE CHE STAMPA LA GRIGLIA FACILE
   if (yourSelect.options[yourSelect.selectedIndex].value === "1") {
     let colNumber = 100;
@@ -19,29 +20,12 @@ function stampareGriglia() {
       "class",
       "row justify-content-center align-content-center"
     );
-    let cols = creaColonne(colNumber);
+    let cols = creaColonne(colNumber, "size-easy");
     row.innerHTML = cols;
     app.append(row);
-
-    function creaColonne(numerocolonne) {
-      let cols = "";
-      let numeriusati = [];
-      let index = 1;
-      while (numeriusati.length < numerocolonne) {
-        let numeroCella = index;
-        if (!numeriusati.includes(numeroCella)) {
-          numeriusati.push(numeroCella);
-          cols += `
-                <div class="size-easy black-border text-center">${numeroCella}</div>
-                `;
-        }
-        index++;
-      }
-      return cols;
-    }
   }
 
-  //CONDIZIONE CHE STAMPA LA GRIGLIA FACILE
+  //CONDIZIONE CHE STAMPA LA GRIGLIA MEDIA
   else if (yourSelect.options[yourSelect.selectedIndex].value === "2") {
     let colNumber = 81;
     let colperRow = Math.sqrt(colNumber);
@@ -56,26 +40,9 @@ function stampareGriglia() {
       "class",
       "row justify-content-center align-content-center"
     );
-    let cols = creaColonne(colNumber);
+    let cols = creaColonne(colNumber, "size-medium");
     row.innerHTML = cols;
     app.append(row);
-
-    function creaColonne(numerocolonne) {
-      let cols = "";
-      let numeriusati = [];
-      let index = 1;
-      while (numeriusati.length < numerocolonne) {
-        let numeroCella = index;
-        if (!numeriusati.includes(numeroCella)) {
-          numeriusati.push(numeroCella);
-          cols += `
-                <div class="size-medium black-border text-center">${numeroCella}</div>
-                `;
-        }
-        index++;
-      }
-      return cols;
-    }
   }
 
   // CONDIZIONE CHE STAMPA LA GRIGLIA DIFFICILE
@@ -93,25 +60,32 @@ function stampareGriglia() {
       "class",
       "row justify-content-center align-content-center"
     );
-    let cols = creaColonne(colNumber);
+    let cols = creaColonne(colNumber, "size-hard");
     row.innerHTML = cols;
     app.append(row);
-
-    function creaColonne(numerocolonne) {
-      let cols = "";
-      let numeriusati = [];
-      let index = 1;
-      while (numeriusati.length < numerocolonne) {
-        let numeroCella = index;
-        if (!numeriusati.includes(numeroCella)) {
-          numeriusati.push(numeroCella);
-          cols += `
-                <div class="size-hard black-border text-center">${numeroCella}</div>
-                `;
-        }
-        index++;
-      }
-      return cols;
-    }
   }
+}
+
+// FUNZIONE CHE CREA LE COLONNE
+function creaColonne(numerocolonne, dimensione) {
+  let cols = "";
+  let numeriusati = [];
+  let index = 1;
+  while (numeriusati.length < numerocolonne) {
+    let numeroCella = index;
+    if (!numeriusati.includes(numeroCella)) {
+      numeriusati.push(numeroCella);
+      cols += `
+              <div id="${numeroCella}" onclick="myFunction(${numeroCella})" class="box ${dimensione} black-border text-center">${numeroCella}</div>
+              `;
+    }
+    index++;
+  }
+  return cols;
+}
+
+// FUNZIONE CHE CAMBIA BACKGROUND AL CLICK
+function myFunction(casella) {
+  console.log(casella);
+  document.getElementById(casella).classList.add("bg-blue");
 }
